1ace7c8aec7a33c4a5247334adef891ca57b977e
Diego <107697070+DiegoSouza01@users.noreply.github.com>
2025-12-23 01:12:21 -0300

    Create sessions table migration

--- infra/migrations/1764552583152_test-migration.js
+++ infra/migrations/1764552583152_test-migration.js
@@ -1,7 +1,40 @@
-/* eslint-disable camelcase */
+exports.up = (pgm) => {
+  pgm.createTable("sessions", {
+    id: {
+      type: "uuid",
+      primaryKey: true,
+      default: pgm.func("gen_random_uuid()"),
+    },
 
-exports.shorthands = undefined;
+    token: {
+      type: "varchar(96)",
+      notNull: true,
+      unique: true,
+    },
 
-exports.up = (pgm) => {};
+    user_id: {
+      type: "uuid",
+      notNull: true,
+    },
 
-exports.down = (pgm) => {};
+    // Why timestamp with timezone? https://justatheory.com/2012/04/postgres-use-timestamptz/
+    expires_at: {
+      type: "timestamptz",
+      notNull: true,
+    },
+
+    created_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
+    },
+
+    updated_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
+    },
+  });
+};
+
+exports.down = false;
