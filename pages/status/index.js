5ca1af2e5ff1a4be3e824dda1efc0d00b25e6361
Diego <107697070+DiegoSouza01@users.noreply.github.com>
2026-04-14 21:20:31 -0300

    Convert status API to getServerSideProps
    Refactor status page to use getServerSideProps for data fetching and update response structure.

--- pages/status/index.js
+++ pages/status/index.js
@@ -1,41 +1,30 @@
-import { createRouter } from "next-connect";
-import database from "infra/database";
-import controller from "infra/controller.js";
+import database from "../../infra/database";
 
-const router = createRouter();
-
-router.get(getHandler);
-
-export default router.handler(controller.errorHandlers);
-
-async function getHandler(request, response) {
+export async function getServerSideProps() {
   const updatedAt = new Date().toISOString();
 
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
-  const databaseMaxConnectionsResult = await database.query(
-    "SHOW max_connections;",
-  );
-  const databaseMaxConnectionsValue =
-    databaseMaxConnectionsResult.rows[0].max_connections;
-
-  const databaseName = process.env.POSTGRES_DB;
-  const databaseOpenedConnectionsResult = await database.query({
-    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
-    values: [databaseName],
-  });
-  const databaseOpenedConnectionsValue =
-    databaseOpenedConnectionsResult.rows[0].count;
-
-  response.status(200).json({
-    updated_at: updatedAt,
-    dependencies: {
-      database: {
-        version: databaseVersionValue,
-        max_connections: parseInt(databaseMaxConnectionsValue),
-        opened_connections: databaseOpenedConnectionsValue,
+  return {
+    props: {
+      statusData: {
+        updated_at: updatedAt,
+        database_version: databaseVersionValue,
       },
     },
-  });
+  };
+}
+
+export default function StatusPage({ statusData }) {
+  return (
+    <div>
+      <h1>Status do Sistema</h1>
+      <p>Última atualização: {statusData.updated_at}</p>
+      <p>Versão do Postgres: {statusData.database_version}</p>
+      
+      <hr />
+      <pre>{JSON.stringify(statusData, null, 2)}</pre>
+    </div>
+  );
 }
